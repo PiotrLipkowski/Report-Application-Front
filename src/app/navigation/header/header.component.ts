8d3aeff975bc5c30232710f257d178eb2a99cdab
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2019-02-03 02:44:05 +0100

    fix authority (now appear)

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { TokenStorageService } from '../../auth/token-storage.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private roles: string[];
+  private authority: string;
 
   @Output() public sidenavToggle = new EventEmitter();
 
-  constructor() { }
+  constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.roles = this.tokenStorage.getAuthorities();
+      this.roles.every(role => {
+        if (role === 'ROLE_ADMIN') {
+          this.authority = 'admin';
+          return false;
+        } else if (role === 'ROLE_PM') {
+          this.authority = 'pm';
+          return false;
+        }
+        this.authority = 'user';
+        return true;
+      });
+    }
   }
 
   public onToggleSidenav = () => {
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { TokenStorageService } from '../../auth/token-storage.service';
+
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+  private roles: string[];
+  private authority: string;
+
   @Output() sidenavClose = new EventEmitter();
 
-  constructor() { }
+  constructor(private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.roles = this.tokenStorage.getAuthorities();
+      this.roles.every(role => {
+        if (role === 'ROLE_ADMIN') {
+          this.authority = 'admin';
+          return false;
+        } else if (role === 'ROLE_PM') {
+          this.authority = 'pm';
+          return false;
+        }
+        this.authority = 'user';
+        return true;
+      });
+    }
   }
 
   public onSidenavClose = () => {
